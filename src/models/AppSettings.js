2d0f7e8b0fa3748a88e6c0c17b6690f68920ae0b
towromanterry-prog <towromanterry@gmail.com>
2026-02-11 19:26:40 +0300

    Update AppSettings.js

---
+++ src/models/AppSettings.js
@@ -0,0 +1,30 @@
+export default class AppSettings {
+  constructor({
+    id = 'global', // Обычно настройки хранятся в одном документе
+    theme = 'light',
+    currency = 'RUB',
+    language = 'ru'
+  } = {}) {
+    this.id = id;
+    this.theme = theme;
+    this.currency = currency;
+    this.language = language;
+  }
+
+  clone() {
+    return new AppSettings({ ...this });
+  }
+}
+
+export const settingsConverter = {
+  toFirestore: (settings) => ({
+    theme: settings.theme,
+    currency: settings.currency,
+    language: settings.language
+  }),
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new AppSettings({ id: snapshot.id, ...data });
+  }
+};
+
